ff04e6ddbc2c8ef5ea120b61f7f8a8f5ed8d9a7c
Anish Shahi <ashahi322@gmail.com>
2023-07-05 06:46:37 +0545

    quiz app completed and all issue fixed

--- screens/Result.js
+++ screens/Result.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
-const Result = ({ navigation }) => {
+const Result = ({ navigation, route }) => {
+  const { score } = route.params;
+  const resultBanner =
+    score > 40
+      ? "https://img.freepik.com/free-vector/men-holding-up-winning-trophy-hand-drawn-sketch-vector-illustration_460848-14484.jpg?w=740&t=st=1688518698~exp=1688519298~hmac=0ca9909b69c5f011f7c56d24b45e018b209fef67e71632ea526795c29df323da"
+      : "https://img.freepik.com/free-vector/oh-no-concept-illustration_114360-8780.jpg?w=740&t=st=1688518728~exp=1688519328~hmac=19fb56e454b3229864fe9404dfe7227d5c0bbc776742a3fba385e4e8d6539de2";
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.title}>Result</Text>
+        <Text style={styles.scoreText}>Your final score is {score}</Text>
       </View>
       <View style={styles.bannerContainer}>
         <Image
           source={{
-            uri: "https://img.freepik.com/free-vector/online-test-concept-illustration_114360-5456.jpg?w=740&t=st=1688432283~exp=1688432883~hmac=9e491e45249050896c75577524793460c8935955473a9891fb133470a5cf3487",
+            uri: resultBanner,
           }}
           style={styles.banner}
           resizeMode="contain"
@@ -39,6 +45,7 @@ const styles = StyleSheet.create({
   bannerContainer: {
     justifyContent: "center",
     alignItems: "center",
+    marginTop: 20,
   },
   button: {
     alignItems: "center",
@@ -49,6 +56,7 @@ const styles = StyleSheet.create({
     elevation: 3,
     backgroundColor: "blue",
     marginBottom: 35,
+    marginTop: 50,
   },
   buttonText: {
     color: "white",
@@ -58,6 +66,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 36,
     fontWeight: "600",
+  },
+  scoreText: {
+    fontSize: 24,
+    alignSelf: "center",
   },
 });
 
